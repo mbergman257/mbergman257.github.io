45cc81c5e61ebf236dccd93aad94ee5ec5924108
mbergman257 <mbergman257@gmail.com>
2025-04-01 00:07:24 +0000

    Deploying to gh-pages from @ mbergman257/mbergman257.github.io@3e3018bb38b5a8aa7b3f398859b228da4cad9787 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,51 +57,31 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
+            },},{id: "projects-sample-project",
+          title: 'Sample Project',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-autonomous-systems-course-redesign",
+          title: 'Autonomous Systems Course Redesign',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/autsys/";
+            },},{id: "projects-hierarchical-classification",
+          title: 'Hierarchical Classification',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/classifier/";
+            },},{id: "projects-wildlife-detection",
+          title: 'wildlife detection',
+          description: "another",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/detection/";
+            },},{id: "projects-docking-station-detection",
+          title: 'Docking Station Detection',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/docking/";
             },},{
         id: 'social-email',
         title: 'email',
